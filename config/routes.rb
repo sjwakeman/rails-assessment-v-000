2ad53f39415dd9475bf4be5c39f9d7fe253a3e8a
Scott Wakeman <github email address>
2018-04-20 22:41:43 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   #devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   resources :trainers
   #resources :users#
   resources :training_sessions
   get '/auth/facebook/callback' => 'sessions#create'
-
 
   root 'welcome#home'
 end
