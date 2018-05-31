299a865609b4d6172f6e2dbee9d47210e2be8423
Scott Wakeman <github email address>
2018-05-31 07:58:48 -0700

    Done.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,6 +1,14 @@
 class ClientsController < ApplicationController
   before_action :authenticate_client, only: [:show]
 
+  def index
+    @clients = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
   def new
     @client = Client.new(client_params)
     if @client.save
@@ -18,10 +26,23 @@ class ClientsController < ApplicationController
     end
   end
 
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    @client.update(params.require(:client).permit(:name))
+      redirect_to client_path(@client)
+  end
 
   private
 
   def client_params
     params.require(:client).permit(:name, :email)
+  end
+
+  def set_client
+    @client = Client.find(params[:id])
   end
 end
