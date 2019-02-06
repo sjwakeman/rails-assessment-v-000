c2b58e24cebf7a8d3017c44f7cfeff4bb8a72b49
sjwakean <swakeman@ca.rr.com>
2019-02-05 16:45:29 -0800

    email vaildation on client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -8,6 +8,7 @@ class Client < ActiveRecord::Base
 
   validates :name, presence: { message: "must be given" }#true
   validates :home_phone, :work_phone, :smart_phone, length: { is: 12, message: "format XXX-XXX-XXXX"}, allow_blank: true
+  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   #validates :name, uniqueness: true
 
   def client_name=(name)
